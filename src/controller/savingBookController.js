6e8a3907281472f31b601e62dd923f2d611c1460
kenduog <82100382+kenduog@users.noreply.github.com>
2023-04-10 16:00:54 +0700

    fix page and controller history page

--- src/controller/savingBookController.js
+++ src/controller/savingBookController.js
@@ -305,48 +305,57 @@ let getListSavingBook = async (userID) => {
   // add global List Saving Book
   return listSavingBook[0];
 };
-//How many posts we want to show on each page
-const resultsPerPage = process.env.NUMBER_PAGE_HISTORY;
+
 let getHistoryPage = async (req, res) => {
-  let [result] = await pool.execute("CALL getHistory(?,1,?)", [
-    generalInfo.glbUser.id,
-    resultsPerPage,
-  ]);
-  const numOfResults = result[0][0].totalPage;
-  const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
-  let page = req.query.page ? Number(req.query.page) : 1;
-  if (page > numberOfPages) {
-    res.redirect("history?page=" + encodeURIComponent(numberOfPagess));
-  } else if (page < 1) {
-    res.redirect("history?page=" + encodeURIComponent("1"));
+  try {
+    //How many posts we want to show on each page
+    const resultsPerPage = process.env.NUMBER_PAGE_HISTORY;
+    let [result] = await pool.execute("CALL getHistory(?,1,?)", [
+      generalInfo.glbUser.id,
+      resultsPerPage,
+    ]);
+    const numOfResults = result[0][0].totalPage;
+    const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
+    let page = req.query.page ? Number(req.query.page) : 1;
+    if (page > numberOfPages) {
+      res.redirect("history?page=" + encodeURIComponent(numberOfPagess));
+    } else if (page < 1) {
+      res.redirect("history?page=" + encodeURIComponent("1"));
+    }
+    //Determine the SQL LIMIT starting number
+    const startingLimit = (page - 1) * resultsPerPage;
+    //Get the relevant number of POSTS for this starting page
+    [result] = await pool.execute("CALL getHistory(?,?,?)", [
+      generalInfo.glbUser.id,
+      startingLimit,
+      resultsPerPage,
+    ]);
+    let iterator = page - 5 < 1 ? 1 : page - 5;
+    let endingLink =
+      iterator + 9 <= numberOfPages
+        ? iterator + 9
+        : page + (numberOfPages - page);
+    if (endingLink < page + 4) {
+      iterator -= page + 4 - numberOfPages;
+    }
+    return res.render("history.ejs", {
+      message: req.flash("status"),
+      status: "success",
+      generalInfo: generalInfo,
+      listSavingBookHistory: result[0],
+      active: "history",
+      page,
+      iterator,
+      endingLink,
+      numberOfPages,
+    });
+  } catch (error) {
+    req.flash("danger", error);
+    return res.render("login.ejs", {
+      message: req.flash("danger"),
+      status: "danger",
+    });
   }
-  //Determine the SQL LIMIT starting number
-  const startingLimit = (page - 1) * resultsPerPage;
-  //Get the relevant number of POSTS for this starting page
-  [result] = await pool.execute("CALL getHistory(?,?,?)", [
-    generalInfo.glbUser.id,
-    startingLimit,
-    resultsPerPage,
-  ]);
-  let iterator = page - 5 < 1 ? 1 : page - 5;
-  let endingLink =
-    iterator + 9 <= numberOfPages
-      ? iterator + 9
-      : page + (numberOfPages - page);
-  if (endingLink < page + 4) {
-    iterator -= page + 4 - numberOfPages;
-  }
-  return res.render("history.ejs", {
-    message: req.flash("status"),
-    status: "success",
-    generalInfo: generalInfo,
-    listSavingBookHistory: result[0],
-    active: "history",
-    page,
-    iterator,
-    endingLink,
-    numberOfPages,
-  });
 };
 
 let getPayPage = async (req, res) => {
